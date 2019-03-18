487bb59d7acc698b9d9f4a1c75a8772cfac6ae00
Jonathan <jmitchel3@gatech.edu>
2019-03-18 17:43:03 -0500

    Developed feature selection code more. Needed function to discretize data first. Not tested yet.

--- feat_select.py
+++ feat_select.py
@@ -1,22 +1,39 @@
-# import pymrmr
+import pandas as pd
+import pymrmr
 # import mifs
-import mrmr
+# import mrmr
 
-def select_features(X,y):
-    ranks = mrmr(X,y,6)
-    # # define MI_FS feature selection method
-    # feat_selector = mifs.MutualInformationFeatureSelector('MRMR')
-    #
-    # # find all relevant features
-    # feat_selector.fit(X, y)
-    #
-    # # check selected features
-    # selected = feat_selector.support_
-    #
-    # # check ranking of features
-    # ranks = feat_selector.ranking_
-    #
-    # # # call transform() on X to filter it down to selected features
-    # # X_filtered = feat_selector.transform(X)
+def select_features(X,y,modality):
+    #combine response with features to one dataframe [y,X]
+    z = pd.concat([y,X],axis=1)
+    z = discretize(z,modality)
 
-    return (ranks)
+    # return (features)
+
+def discretize(df,modality): #features need to be -2,0,2 and response needs to be -1,1
+    if modality == 'CNV':
+        df[df == -1] = -2
+        df[df == 0] = 0
+        df[df == 1] = 2
+    else:
+        # get mean and standard deviation each row column (since features are now columns)
+        std = df.std(axis=0)
+        av = df.mean(axis=0)
+
+        df[df < av-std] = -2
+        df[df > av+std] = 2
+        df[df > av-std and df < av+std] = 0
+
+
+
+# # initialize list of lists
+# data1 = [[5, 5], [5, 5], [0, 0]]
+# data2 = [[0, 0], [0, 0], [1, 1]]
+#
+# # Create the pandas DataFrame
+# df1 = pd.DataFrame(data1, columns = ['c1', 'c2'])
+# df2 = pd.DataFrame(data2, columns = ['c1', 'c2'])
+# print(df2)
+# cool = df2.std(axis=0)
+# cool2 = df2.mean(axis=0)
+# print(cool2)

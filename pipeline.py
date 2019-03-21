7189c34895747a9da2ed8ae05cad11b6d64e100b
Jonathan <jmitchel3@gatech.edu>
2019-03-21 12:59:44 -0500

    using RobustScaler instead of dividing by max of each gene. Seemed to have minimal effect on classifier accuracy

--- pipeline.py
+++ pipeline.py
@@ -2,6 +2,8 @@ import pandas as pd
 import os
 from sklearn.model_selection import train_test_split
 from train_ind import tr_ind
+from sklearn import preprocessing
+import numpy as np
 
 os.chdir("C:\\Users\\jonat\\Documents\\Spring 2019 Classes\\4813\\BHI_data")
 labels = pd.read_csv('Label_selected.csv')
@@ -33,10 +35,6 @@ meth = meth[lbl['case_id']] #indexes out the correct patient samples
 gene = gene.loc[~(gene==0).all(axis=1)]
 miRNA = miRNA.loc[~(miRNA==0).all(axis=1)]
 
-# normalizing gene expression and miRNA datasets
-gene = gene.div(gene.max(axis=1), axis=0)
-miRNA = miRNA.div(miRNA.max(axis=1), axis=0)
-
 # splitting labels into train set and validation set
 train_labels, test_labels, train_class, test_class = train_test_split(
     lbl['case_id'], lbl, test_size=0.15, random_state=42)
@@ -50,12 +48,27 @@ CNV_train = CNV[train_labels].T
 CNV_test = CNV[test_labels].T
 meth_train = meth[train_labels].T
 meth_test = meth[test_labels].T
-train_class = train_class.set_index('case_id') #changes first column to be indices
-tr_ind(miRNA_train,train_class,'miRNA')
 
 
+# # normalizing gene expression and miRNA datasets
+# gene = gene.div(gene.max(axis=1), axis=0)
+# miRNA = miRNA.div(miRNA.max(axis=1), axis=0)
 
-#test commit
+miRNA_train_copy = pd.DataFrame(miRNA_train, copy=True) # copies the original dataframe
+miRNA_scaler = preprocessing.RobustScaler().fit(miRNA_train)
+miRNA_train = miRNA_scaler.transform(miRNA_train)
+miRNA_train = pd.DataFrame(miRNA_train,columns=list(miRNA_train_copy)).set_index(miRNA_train_copy.index.values)
+
+gene_train_copy = pd.DataFrame(gene_train, copy=True) # copies the original dataframe
+gene_scaler = preprocessing.RobustScaler().fit(gene_train)
+gene_train = gene_scaler.transform(gene_train)
+gene_train = pd.DataFrame(gene_train,columns=list(gene_train_copy)).set_index(gene_train_copy.index.values)
+
+train_class = train_class.set_index('case_id') # changes first column to be indices
+
+tr_ind(miRNA_train,train_class,'miRNA') # fs + classification cv
+
+
 
 
 
--- train_ind.py
+++ train_ind.py
@@ -2,6 +2,7 @@ from sklearn import svm
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import StratifiedKFold
 from feat_select import select_features
+from feat_select import discretize
 import pandas as pd
 import csv
 import numpy as np
@@ -42,20 +43,25 @@ def tr_ind(X,y,type):
         # feature selection for train data
         X_train2 = pd.DataFrame(X_train, copy=True)  # copies the original feature dataframe
         y_train2 = pd.DataFrame(y_train, copy=True)  # copies the original feature dataframe
-        feat_selected = select_features(X_train, y_train, type, 30)
+        feat_selected = select_features(X_train, y_train, type, 20)
         fold_feats.append(feat_selected)
     print(fold_feats)
 
     # compute average accuracy for each possible parameter combination
+    tot_acc = []
     for k in kernels:
         for c in c_values:
-            for num_feats in [10,20,30]:
+            for num_feats in [10,20]:
                 count = 0
+                acc = []
                 for train,test in kf.split(X,y):
                     tr_ndx = X.index.values[train]
                     te_ndx = X.index.values[test]
                     X_train, X_test = X.loc[tr_ndx,:], X.loc[te_ndx,:]
                     y_train, y_test = y.loc[tr_ndx,:], y.loc[te_ndx,:]
+
+                    # X_train, y_train = discretize(X_train,y_train,'miRNA',2.5) #SPECIAL TEST ONLY
+                    # X_test, y_test = discretize(X_test,y_test,'miRNA',2.5) #SPECIAL TEST ONLY
 
                     X_train = X_train[fold_feats[count][0:num_feats]] # shrinks train fold to have selected features only
                     X_test = X_test[fold_feats[count][0:num_feats]] # shrinks train fold to have selected features only
@@ -63,9 +69,12 @@ def tr_ind(X,y,type):
                     # start of classification
                     clf = svm.SVC(C=c,gamma="auto",kernel=k)
                     clf.fit(X_train, y_train.values.ravel()) #can also try X_train here if alter it first
-                    print('fold number:%d, kernel:%s, c-value:%f, num feats:%d' % (count+1,k,c,num_feats))
-                    print(clf.score(X_test,y_test))
+                    acc.append(clf.score(X_test,y_test))
                     count = count + 1 # iterates to the next fold
+                print('kernel:%s, c-value:%f, num feats:%d' % (k, c, num_feats))
+                print(np.mean(acc))
+                tot_acc.append(np.mean(acc))
+    print(np.max(tot_acc))
 
 
 
